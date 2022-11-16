6fefc47c76e92c02cc3ed69d7fdb468e5ce2c0c1
Lucas Josino <contact@lucasjosino.com>
2022-11-16 11:32:33 -0300

    Add all initial files

---
+++ src/main/java/com/lucasjosino/hawapi/HawAPIApplication.java
@@ -0,0 +1,11 @@
+package com.lucasjosino.hawapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawAPIApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HawAPIApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lucasjosino/hawapi/HawAPIApplicationTests.java
@@ -0,0 +1,11 @@
+package com.lucasjosino.hawapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HawAPIApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
