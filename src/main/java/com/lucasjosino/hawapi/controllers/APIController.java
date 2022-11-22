9dd34472ced67f9e7b1bef604cf2c8d53c17b8cd
Lucas Josino <contact@lucasjosino.com>
2022-11-22 08:19:26 -0300

    Models: Setup (#1)
    - Update pom structure;
    - Convert APIModel variables to public and static;
    - Add models.

--- src/main/java/com/lucasjosino/hawapi/controllers/APIController.java
+++ src/main/java/com/lucasjosino/hawapi/controllers/APIController.java
@@ -8,19 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class APIController {
-
-    public APIModel apiModel = new APIModel(
-            "HawAPI",
-            "A free and open source API for Stranger Things.",
-            "v1",
-            "https://github.com/LucJosin/HawAPI",
-            "https://hawapi.theproject.id",
-            "https://hawapi.theproject.id/api/v1",
-            "/api/v1"
-    );
-
-    @GetMapping()
-    public APIModel getAPIInfo() {
-        return apiModel;
+    @GetMapping
+    public String getAPIInfo() {
+        return APIModel.name;
     }
 }
---
+++ src/main/java/com/lucasjosino/hawapi/models/base/BaseModel.java
@@ -0,0 +1,98 @@
+package com.lucasjosino.hawapi.models.base;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.UUID;
+
+@MappedSuperclass
+abstract public class BaseModel {
+    @Id
+    @Column
+    private int id;
+
+    @Column
+    private UUID uuid;
+
+    @Column
+    private String href;
+
+    @Column
+    private String[] awards;
+
+    @Column
+    private String[] sources;
+
+    @Column
+    private String[] extras;
+
+    @Column(name = "created_at")
+    private String createdAt;
+
+    @Column(name = "updated_at")
+    private String updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public String[] getAwards() {
+        return awards;
+    }
+
+    public void setAwards(String[] awards) {
+        this.awards = awards;
+    }
+
+    public String[] getSources() {
+        return sources;
+    }
+
+    public void setSources(String[] sources) {
+        this.sources = sources;
+    }
+
+    public String[] getExtras() {
+        return extras;
+    }
+
+    public void setExtras(String[] extras) {
+        this.extras = extras;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String created_at) {
+        this.createdAt = created_at;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updated_at) {
+        this.updatedAt = updated_at;
+    }
+}
