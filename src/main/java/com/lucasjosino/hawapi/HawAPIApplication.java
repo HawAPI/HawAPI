d4bee4cddaf487a29a15f0d3122e0384baab3e62
Lucas Josino <contact@lucasjosino.com>
2022-11-24 12:19:57 -0300

    Enalbe JPA and Add `/ping` route

--- src/main/java/com/lucasjosino/hawapi/HawAPIApplication.java
+++ src/main/java/com/lucasjosino/hawapi/HawAPIApplication.java
@@ -2,8 +2,10 @@ package com.lucasjosino.hawapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class HawAPIApplication {
     public static void main(String[] args) {
         SpringApplication.run(HawAPIApplication.class, args);
--- src/main/java/com/lucasjosino/hawapi/controllers/APIController.java
+++ src/main/java/com/lucasjosino/hawapi/controllers/APIController.java
@@ -1,15 +1,22 @@
 package com.lucasjosino.hawapi.controllers;
 
-import com.lucasjosino.hawapi.models.APIModel;
+import com.lucasjosino.hawapi.models.dto.OpenAPIDtoModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/")
 public class APIController {
     @GetMapping
-    public String getAPIInfo() {
-        return APIModel.name;
+    public ResponseEntity<OpenAPIDtoModel> getAPIInfo() {
+        OpenAPIDtoModel openAPIDto = new OpenAPIDtoModel();
+        return ResponseEntity.ok(openAPIDto);
+    }
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> getPing() {
+        return ResponseEntity.ok("Pong");
     }
 }
