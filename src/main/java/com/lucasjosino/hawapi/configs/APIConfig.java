84e8c2da442105acb5f72adcfb46b474a92e29b1
Lucas Josino <contact@lucasjosino.com>
2022-11-24 12:14:12 -0300

    Add docs config

---
+++ src/main/java/com/lucasjosino/hawapi/configs/APIConfig.java
@@ -0,0 +1,27 @@
+package com.lucasjosino.hawapi.configs;
+
+public class APIConfig {
+    public static final String title = "HawAPI";
+
+    public static final String description = "A free and open source API for Stranger Things.";
+
+    public static final String version = "v1.0.0";
+
+    public static final String url = "https://hawapi.theproject.id";
+
+    public static final String docs = "https://hawapi.theproject.id/docs";
+
+    public static final String github = "https://github.com/LucJosin/HawAPI";
+
+    public static final String githubUrl = "https://github.com/LucJosin/HawAPI";
+
+    public static final String apiUrl = "https://hawapi.theproject.id/api/v1";
+
+    public static final String apiVersion = "v1";
+
+    public static final String apiBaseUrl = "/api/v1";
+
+    public static final String license = "BSD 3-Clause \"New\" or \"Revised\" License";
+
+    public static final String licenseUrl = "https://github.com/LucJosin/HawAPI/blob/main/LICENSE";
+}
