fd80bf5c2e05f85cf363a57bdc5876ab16f67bc6
Lucas Josino <contact@lucasjosino.com>
2022-11-24 12:39:23 -0300

    Fix `/api` prefix

--- src/main/java/com/lucasjosino/hawapi/controllers/APIController.java
+++ src/main/java/com/lucasjosino/hawapi/controllers/APIController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("${apiPrefix}/")
 public class APIController {
     @GetMapping
     public ResponseEntity<OpenAPIDtoModel> getAPIInfo() {
