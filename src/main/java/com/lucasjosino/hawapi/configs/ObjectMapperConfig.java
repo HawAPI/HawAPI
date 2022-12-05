a87ecc1632998c388dddebcd3eb749505687432f
Lucas Josino <contact@lucasjosino.com>
2022-12-05 08:40:24 -0300

    Add `json-patch` dependence and ObjectMapperConfig

---
+++ src/main/java/com/lucasjosino/hawapi/configs/ObjectMapperConfig.java
@@ -0,0 +1,21 @@
+package com.lucasjosino.hawapi.configs;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper mapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+}
