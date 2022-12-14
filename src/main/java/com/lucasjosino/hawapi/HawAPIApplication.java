e9b83d2d41b7a641f94d0bfb2c660e3f6724484e
Lucas Josino <contact@lucasjosino.com>
2022-12-14 12:12:00 -0300

    Endpoints: GET (#20)
    - Add security dependency to pom.xml;
    - Enable web security;
    - Add web security cofiguration.

--- src/main/java/com/lucasjosino/hawapi/HawAPIApplication.java
+++ src/main/java/com/lucasjosino/hawapi/HawAPIApplication.java
@@ -3,9 +3,11 @@ package com.lucasjosino.hawapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableWebSecurity
 public class HawAPIApplication {
     public static void main(String[] args) {
         SpringApplication.run(HawAPIApplication.class, args);
---
+++ src/main/java/com/lucasjosino/hawapi/configs/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.lucasjosino.hawapi.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors()
+                .and()
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                // Swagger (Only available on Debug)
+                .antMatchers(HttpMethod.GET, "/swagger-ui/**", "/api-docs/**").permitAll()
+                // Errors
+                .antMatchers(HttpMethod.GET, "/error").permitAll()
+                // API
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+                // Others endpoints
+                .anyRequest().authenticated()
+                .and()
+                .build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
