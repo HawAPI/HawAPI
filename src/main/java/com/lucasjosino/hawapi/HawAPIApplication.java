8c20d3a6f89a190018b8f1f6991899ac08e8412b
Lucas Josino <contact@lucasjosino.com>
2022-12-19 20:29:26 -0300

    Add default rate limit using bucket4j and hazelcast

--- src/main/java/com/lucasjosino/hawapi/HawAPIApplication.java
+++ src/main/java/com/lucasjosino/hawapi/HawAPIApplication.java
@@ -2,12 +2,14 @@ package com.lucasjosino.hawapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableWebSecurity
+@EnableCaching
 public class HawAPIApplication {
     public static void main(String[] args) {
         SpringApplication.run(HawAPIApplication.class, args);
