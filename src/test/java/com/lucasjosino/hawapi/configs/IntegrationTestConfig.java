e0c83b9ecc3060d589a48c2dfc1679ac3ab63aa8
Lucas Josino <contact@lucasjosino.com>
2023-01-04 18:16:08 -0300

    Fix broken test (IntegrationTestConfig)

--- src/test/java/com/lucasjosino/hawapi/configs/IntegrationTestConfig.java
+++ src/test/java/com/lucasjosino/hawapi/configs/IntegrationTestConfig.java
@@ -4,6 +4,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.lang.annotation.*;
+
 /**
  * Annotation that can be applied to an integration test class.
  *
@@ -11,8 +13,11 @@ import org.springframework.test.context.ActiveProfiles;
  * @see ActiveProfiles
  * @see SpringBootTest
  */
+@Inherited
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public @interface IntegrationTestConfig {
 }
