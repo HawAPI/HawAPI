0232523de46102fb95b8c426b64b2117b941af0e
Lucas Josino <contact@lucasjosino.com>
2023-01-20 13:18:03 -0300

    Update IndexFallbackResourceResolver

--- src/main/java/com/lucasjosino/hawapi/resolvers/IndexFallbackResourceResolver.java
+++ src/main/java/com/lucasjosino/hawapi/resolvers/IndexFallbackResourceResolver.java
@@ -20,22 +20,32 @@ public class IndexFallbackResourceResolver extends PathResourceResolver {
             List<? extends Resource> locations,
             ResourceResolverChain chain
     ) {
-        // Redirect all pages to '/index.html'
-        //
-        // E.g:
-        //  * /        -> /index.html
-        //  * /try-it  -> /try-it/index.html
-        //  * /docs    -> /docs/index.html
         Resource resource = super.resolveResourceInternal(request, requestPath, locations, chain);
         boolean isAPage = !requestPath.contains("assets")
                 && !requestPath.contains("data")
                 && !requestPath.contains("resources")
                 && !requestPath.contains("fonts");
 
+        // Redirect all pages to '/index.html'
+        //
+        // E.g:
+        //  * /        -> /index.html
+        //  * /try-it  -> /try-it/index.html
+        //  * /docs    -> /docs/index.html
         if (resource == null && isAPage) {
             resource = super.resolveResourceInternal(
                     request,
                     requestPath + "/index.html",
+                    locations,
+                    chain
+            );
+        }
+
+        // Handler requests for '/resources' (css, js, etc...)
+        if (resource == null && requestPath.contains("resources")) {
+            resource = super.resolveResourceInternal(
+                    request,
+                    "docs/" + requestPath,
                     locations,
                     chain
             );
