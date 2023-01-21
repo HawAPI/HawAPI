43cb6a06361e300fe1da187c5fb817e065b35bf5
Lucas Josino <contact@lucasjosino.com>
2023-01-20 21:54:03 -0300

    Website: Config (#36)
    ### Features
    - Add `StaticResourceConfig` and `IndexFallbackResourceResolver`.
    
    ### Fixes
    - Fix spring not redirecting pages to `/index.html`.
    
    ### Changes
    - Remove swagger custom path;
    - Remove rate limit from website.

---
+++ src/main/java/com/lucasjosino/hawapi/configs/StaticResourceConfig.java
@@ -0,0 +1,22 @@
+package com.lucasjosino.hawapi.configs;
+
+import com.lucasjosino.hawapi.resolvers.IndexFallbackResourceResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .setOrder(Ordered.LOWEST_PRECEDENCE)
+                .addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new IndexFallbackResourceResolver());
+    }
+}
---
+++ src/main/java/com/lucasjosino/hawapi/resolvers/IndexFallbackResourceResolver.java
@@ -0,0 +1,56 @@
+package com.lucasjosino.hawapi.resolvers;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.resource.ResourceResolverChain;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@SuppressWarnings("NullableProblems")
+public class IndexFallbackResourceResolver extends PathResourceResolver {
+
+    // Ref: https://stackoverflow.com/a/69647129
+    //
+    // TODO: Review this workaround
+    @Override
+    protected Resource resolveResourceInternal(
+            HttpServletRequest request,
+            String requestPath,
+            List<? extends Resource> locations,
+            ResourceResolverChain chain
+    ) {
+        Resource resource = super.resolveResourceInternal(request, requestPath, locations, chain);
+        boolean isAPage = !requestPath.contains("assets")
+                && !requestPath.contains("data")
+                && !requestPath.contains("resources")
+                && !requestPath.contains("fonts");
+
+        // Redirect all pages to '/index.html'
+        //
+        // E.g:
+        //  * /        -> /index.html
+        //  * /try-it  -> /try-it/index.html
+        //  * /docs    -> /docs/index.html
+        if (resource == null && isAPage) {
+            resource = super.resolveResourceInternal(
+                    request,
+                    requestPath + "/index.html",
+                    locations,
+                    chain
+            );
+        }
+
+        // Handler requests for '/resources' (css, js, etc...)
+        if (resource == null && requestPath.contains("resources")) {
+            resource = super.resolveResourceInternal(
+                    request,
+                    "docs/" + requestPath,
+                    locations,
+                    chain
+            );
+        }
+
+        return resource;
+    }
+}
