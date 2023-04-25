9c450a35c239bf6f08402427941d07fba6759c08
Lucas Josino <contact@lucasjosino.com>
2023-04-25 11:13:58 -0300

    Update base filter

--- src/main/java/com/lucasjosino/hawapi/filters/base/BaseFilter.java
+++ src/main/java/com/lucasjosino/hawapi/filters/base/BaseFilter.java
@@ -2,23 +2,4 @@ package com.lucasjosino.hawapi.filters.base;
 
 abstract public class BaseFilter {
 
-    private String sort;
-
-    private String order;
-
-    public String getSort() {
-        return sort;
-    }
-
-    public void setSort(String sort) {
-        this.sort = sort;
-    }
-
-    public String getOrder() {
-        return order;
-    }
-
-    public void setOrder(String order) {
-        this.order = order;
-    }
 }
