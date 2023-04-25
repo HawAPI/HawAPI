6ec72b4b2e15dbfe34f0074fa74f83864d789750
Lucas Josino <contact@lucasjosino.com>
2023-04-25 10:36:21 -0300

    Add `language` field to BaseTranslationDTO

--- src/main/java/com/lucasjosino/hawapi/models/base/BaseTranslationDTO.java
+++ src/main/java/com/lucasjosino/hawapi/models/base/BaseTranslationDTO.java
@@ -9,4 +9,13 @@ import java.io.Serializable;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 abstract public class BaseTranslationDTO implements Serializable {
 
+    private String language;
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
 }
