600f941e5ed67146f90eeb2a83e6aa6336d66312
Lucas Josino <contact@lucasjosino.com>
2023-05-31 16:16:29 -0300

    Add missing `language` validator

--- src/main/java/com/lucasjosino/hawapi/models/base/BaseTranslationDTO.java
+++ src/main/java/com/lucasjosino/hawapi/models/base/BaseTranslationDTO.java
@@ -3,12 +3,16 @@ package com.lucasjosino.hawapi.models.base;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @MappedSuperclass
 @JsonInclude(JsonInclude.Include.NON_NULL)
 abstract public class BaseTranslationDTO implements Serializable {
 
+    @Size(max = 5)
+    @NotBlank(message = "Field 'language' is required")
     private String language;
 
     public String getLanguage() {
