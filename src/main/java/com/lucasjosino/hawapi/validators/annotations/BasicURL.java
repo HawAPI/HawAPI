1847d1900626dc5b175758e09d7c83ab3d86d5cb
Lucas Josino <contact@lucasjosino.com>
2023-05-31 11:47:48 -0300

    Add BasicURL annotation

---
+++ src/main/java/com/lucasjosino/hawapi/validators/annotations/BasicURL.java
@@ -0,0 +1,36 @@
+package com.lucasjosino.hawapi.validators.annotations;
+
+import com.lucasjosino.hawapi.validators.BasicURLValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+/**
+ * The annotated element must be a valid basic url.
+ */
+@Documented
+@Target({ElementType.FIELD, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = BasicURLValidator.class)
+public @interface BasicURL {
+
+    /**
+     * Define if element must be secure (using https).
+     */
+    boolean secure() default true;
+
+    /**
+     * Define if element must be an image (jpg, jpeg, png).
+     */
+    boolean image() default false;
+
+    /**
+     * Define the error message.
+     */
+    String message() default "Provide a valid URL";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
