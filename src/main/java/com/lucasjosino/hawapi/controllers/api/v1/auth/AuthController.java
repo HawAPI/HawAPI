a09c7850ad7dacdd55ccba4a7a2ea1c04a270765
Lucas Josino <contact@lucasjosino.com>
2023-06-01 11:55:10 -0300

    Add BaseAuthControllerInterface to AuthController

--- src/main/java/com/lucasjosino/hawapi/controllers/api/v1/auth/AuthController.java
+++ src/main/java/com/lucasjosino/hawapi/controllers/api/v1/auth/AuthController.java
@@ -1,26 +1,19 @@
 package com.lucasjosino.hawapi.controllers.api.v1.auth;
 
+import com.lucasjosino.hawapi.controllers.interfaces.BaseAuthControllerInterface;
 import com.lucasjosino.hawapi.exceptions.auth.UserUnauthorizedException;
+import com.lucasjosino.hawapi.models.dto.auth.UserAuthDTO;
 import com.lucasjosino.hawapi.models.dto.auth.UserDTO;
-import com.lucasjosino.hawapi.models.user.UserAuthenticationModel;
-import com.lucasjosino.hawapi.models.user.UserModel;
-import com.lucasjosino.hawapi.services.auth.AuthService;
+import com.lucasjosino.hawapi.models.dto.auth.UserRegistrationDTO;
+import com.lucasjosino.hawapi.services.impl.auth.AuthServiceImpl;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -31,54 +24,34 @@ import javax.validation.Valid;
                 url = "/docs/guides/authentication"
         )
 )
-public class AuthController {
+public class AuthController implements BaseAuthControllerInterface {
 
-    private final AuthService authService;
+    private final AuthServiceImpl service;
 
     @Value("${com.lucasjosino.hawapi.registration.enable}")
     private boolean registrationIsEnable;
 
-    public AuthController(AuthService authService) {
-        this.authService = authService;
+    public AuthController(AuthServiceImpl service) {
+        this.service = service;
     }
 
-    @PostMapping(
-            value = "/register",
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE
-    )
     @Operation(summary = "Register user")
-    @ApiResponse(responseCode = "201", description = "Created")
-    @ApiResponse(responseCode = "429", description = "Too Many Requests", content = @Content(schema = @Schema(hidden = true)))
-    @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true)))
-    public ResponseEntity<UserDTO> register(@Valid @RequestBody UserModel user) {
+    public ResponseEntity<UserDTO> register(UserRegistrationDTO user) {
         if (registrationIsEnable) {
-            return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(user));
+            return ResponseEntity.status(HttpStatus.CREATED).body(service.register(user));
         }
 
         throw new UserUnauthorizedException("Registration is not available at the moment");
     }
 
-    @PostMapping(
-            value = "/authenticate",
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE
-    )
     @Operation(summary = "Authenticate user")
-    @ApiResponse(responseCode = "200", description = "Successful")
-    @ApiResponse(responseCode = "429", description = "Too Many Requests", content = @Content(schema = @Schema(hidden = true)))
-    @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true)))
-    public ResponseEntity<UserDTO> authenticate(@Valid @RequestBody UserAuthenticationModel userAuth) {
-        return ResponseEntity.ok(authService.authenticate(userAuth));
+    public ResponseEntity<UserDTO> authenticate(UserAuthDTO userAuth) {
+        return ResponseEntity.ok(service.authenticate(userAuth));
     }
 
-    @PostMapping(value = "/delete", consumes = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Delete user")
-    @ApiResponse(responseCode = "204", description = "No Content")
-    @ApiResponse(responseCode = "429", description = "Too Many Requests", content = @Content(schema = @Schema(hidden = true)))
-    @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(hidden = true)))
-    public ResponseEntity<Void> delete(@Valid @RequestBody UserAuthenticationModel userAuth) {
-        authService.delete(userAuth);
+    public ResponseEntity<Void> delete(UserAuthDTO userAuth) {
+        service.delete(userAuth);
         return ResponseEntity.noContent().build();
     }
 
