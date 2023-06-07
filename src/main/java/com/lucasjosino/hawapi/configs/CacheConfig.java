dd9b17fe3568bb06c1dfdc9e41e0546203f1a7d0
Lucas Josino <contact@lucasjosino.com>
2023-06-07 19:07:59 -0300

    feat(cache): Add CacheConfig and FindAllKeyGenerator

---
+++ src/main/java/com/lucasjosino/hawapi/configs/CacheConfig.java
@@ -0,0 +1,16 @@
+package com.lucasjosino.hawapi.configs;
+
+import com.lucasjosino.hawapi.cache.generator.FindAllKeyGenerator;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CacheConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public KeyGenerator findAllKeyGenerator() {
+        return new FindAllKeyGenerator();
+    }
+}
