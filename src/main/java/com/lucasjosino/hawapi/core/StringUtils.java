3e1d9ccf57af4ed56be16666c61b51ae500c9036
Lucas Josino <contact@lucasjosino.com>
2023-06-12 12:09:22 -0300

    feat: Add docs to core

--- src/main/java/com/lucasjosino/hawapi/core/StringUtils.java
+++ src/main/java/com/lucasjosino/hawapi/core/StringUtils.java
@@ -1,5 +1,11 @@
 package com.lucasjosino.hawapi.core;
 
+/**
+ * A {@link String} utils for HawAPI project
+ *
+ * @author Lucas Josino
+ * @since 1.0.0
+ */
 public class StringUtils {
 
     public static boolean isNullOrEmpty(String str) {
