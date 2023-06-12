d0fae1e486bd50077be82d32cce17e62e314bb41
Lucas Josino <contact@lucasjosino.com>
2023-06-12 07:50:32 -0300

    feat: Add docs to cache

--- src/main/java/com/lucasjosino/hawapi/cache/generator/FindAllKeyGenerator.java
+++ src/main/java/com/lucasjosino/hawapi/cache/generator/FindAllKeyGenerator.java
@@ -1,10 +1,19 @@
 package com.lucasjosino.hawapi.cache.generator;
 
+import com.lucasjosino.hawapi.services.base.BaseService;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.util.StringUtils;
 
 import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
 
+/**
+ * Generate key for 'findAll' cache methods using only params delimited with <strong>comma<strong/>.
+ *
+ * @see BaseService#findAll(Map, List)
+ */
+@SuppressWarnings("NullableProblems")
 public class FindAllKeyGenerator implements KeyGenerator {
 
     @Override
