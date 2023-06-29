92ee75b42e1e422ae930ee537d67094cb7f249b0
Lucas Josino <contact@lucasjosino.com>
2023-06-29 17:30:53 -0300

    feat: Remove unused files and Update repository config

--- src/test/java/com/lucasjosino/hawapi/configs/RepositoryUnitTestConfig.java
+++ src/test/java/com/lucasjosino/hawapi/configs/RepositoryUnitTestConfig.java
@@ -3,6 +3,7 @@ package com.lucasjosino.hawapi.configs;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.lang.annotation.*;
@@ -11,13 +12,12 @@ import java.lang.annotation.*;
  * Annotation that can be applied to a test class to configure a test repository.
  *
  * @see DataJpaTest
- * @see UnitTestConfig
  * @see AutoConfigureTestDatabase
  */
 @Inherited
 @DataJpaTest
 @Transactional
-@UnitTestConfig
+@ActiveProfiles("test")
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
