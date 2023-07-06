0ee3561756ec30075d467f7b7b8a3dd23d885f84
Lucas Josino <contact@lucasjosino.com>
2023-07-06 20:25:25 -0300

    refactor: buildHeaders method

--- src/test/java/com/lucasjosino/hawapi/utils/TestUtils.java
+++ src/test/java/com/lucasjosino/hawapi/utils/TestUtils.java
@@ -11,13 +11,16 @@ import static com.lucasjosino.hawapi.core.StringUtils.isNullOrEmpty;
 public class TestUtils {
 
     public static HttpHeaders buildHeaders(Pageable pageable, Page<UUID> uuids, String language) {
-        return new HttpHeaders() {{
+        HttpHeaders headers = new HttpHeaders() {{
             add("X-Pagination-Page-Index", String.valueOf(pageable.getPageNumber() + 1));
             add("X-Pagination-Page-Size", String.valueOf(pageable.getPageSize()));
             add("X-Pagination-Page-Total", String.valueOf(uuids.getTotalPages()));
             add("X-Pagination-Item-Total", String.valueOf(uuids.getContent().size()));
-            add("Content-Language", isNullOrEmpty(language) ? "en-US" : language);
         }};
+
+        if (!isNullOrEmpty(language)) headers.add("Content-Language", language);
+
+        return headers;
     }
 
     public static HttpHeaders buildHeaders(String language) {
