63abd8e04078b51f617e7723417e37460867964e
Lucas Josino <contact@lucasjosino.com>
2023-08-04 19:44:59 -0300

    fix: Wrong return on `findAllUUIds` method on BaseJpaRepository

--- src/main/java/com/lucasjosino/hawapi/repositories/base/BaseJpaRepository.java
+++ src/main/java/com/lucasjosino/hawapi/repositories/base/BaseJpaRepository.java
@@ -1,6 +1,7 @@
 package com.lucasjosino.hawapi.repositories.base;
 
 import com.lucasjosino.hawapi.models.base.BaseModel;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -13,15 +14,15 @@ import java.util.UUID;
 /**
  * A base JPA Repository interface with all common methods.
  *
- * @param <T> An object that extends {@link BaseModel}
- * @param <Y> An object that represents the entity id
+ * @param <T>  An object that extends {@link BaseModel}
+ * @param <ID> An object that represents the entity id
  * @author Lucas Josino
  * @see JpaSpecificationExecutor
  * @see JpaRepository
  * @since 1.0.0
  */
 @NoRepositoryBean
-public interface BaseJpaRepository<T extends BaseModel, Y> extends JpaRepository<T, Y>, JpaSpecificationExecutor<T> {
+public interface BaseJpaRepository<T extends BaseModel, ID> extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
 
     /**
      * Method to get all items filtering with {@link Pageable}
@@ -30,6 +31,6 @@ public interface BaseJpaRepository<T extends BaseModel, Y> extends JpaRepository
      * @return An {@link List} of {@link UUID}
      * @since 1.0.0
      */
-    @Query("SELECT uuid from #{#entityName}")
-    List<UUID> findAllUUIDs(Pageable pageable);
+    @Query("SELECT e.uuid from #{#entityName} e")
+    Page<ID> findAllUUIDs(Pageable pageable);
 }
